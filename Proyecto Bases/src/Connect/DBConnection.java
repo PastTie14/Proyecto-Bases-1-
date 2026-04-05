a3854d4f4414afdd7b1b966497678e0b50c236d3
Allan <allajox22@gmail.com>
2026-04-05 13:18:10 -0600

    Connection preparations done
    ojdbc6.jar added and connection made

---
+++ Proyecto Bases/src/Connect/DBConnection.java
@@ -0,0 +1,9 @@
+package Connect;
+
+/**
+ *jdbc:oracle:thin:@localhost:1521:Pr1
+ * @author Allan
+ */
+public class DBConnection {
+    
+}
--- Proyecto Bases/src/proyecto/bases/ProyectoBases.java
+++ Proyecto Bases/src/proyecto/bases/ProyectoBases.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package proyecto.bases;
 
 /**
@@ -14,7 +10,7 @@ public class ProyectoBases {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
