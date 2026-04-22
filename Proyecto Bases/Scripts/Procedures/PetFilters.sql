a3b463a18b7bec9e847adeb17e688872bd678edd
Allan <allajox22@gmail.com>
2026-04-21 22:19:52 -0600

    PetFilters finished
    Might need expansion

--- Proyecto Bases/Scripts/Procedures/PetFilters.sql
+++ Proyecto Bases/Scripts/Procedures/PetFilters.sql
@@ -1,11 +1,14 @@
 CREATE OR REPLACE PROCEDURE getFilters (pIdChip IN NUMBER, pIdDistrict IN NUMBER, pIdCanton IN NUMBER,
-                                        pIdProvince IN NUMBER, pIdStatus IN NUMBER, pIdPetType IN NUMBER, 
-                                        pIdRace IN NUMBER, pIdColor IN NUMBER, petCursor OUT SYS_REFCURSOR)
+                                        pIdProvince IN NUMBER, pIdStatus IN NUMBER, pIdPetType IN NUMBER,
+                                        pIdRescuer IN NUMBER, pIdRace IN NUMBER, pIdColor IN NUMBER,
+                                        petCursor OUT SYS_REFCURSOR)
 
 AS
 BEGIN 
     OPEN petCursor FOR
-        SELECT id_chip, id_district, id_canton, id_province, id_status, id_pet_type, id_race, id_color
+        SELECT DISTINCT ic.id_chip, d.id_district, c.id_canton,
+                        pr.id_province, p.id_status, p.id_pet_type,
+                        r.id_race, p.id_rescuer, c.id_color
         FROM pet p
         INNER JOIN identification_chip ic
         ON p.id_pet = ic.id_pet
@@ -17,7 +20,7 @@ BEGIN
         ON d.id_canton = c.id_canton
         
         INNER JOIN province pr
-        ON c_id_province = pr.id_province
+        ON c.id_province = pr.id_province
         
         INNER JOIN status s
         ON p.id_status = s.id_status
@@ -26,7 +29,7 @@ BEGIN
         ON p.id_pet_type = pt.id_pet_type
         
         INNER JOIN race r
-        ON p.id_race = r.id_race
+        ON pt.id_race = r.id_race
         
         INNER JOIN color_x_pet cxp
         ON p.id_pet = cxp.id_pet
@@ -39,7 +42,8 @@ BEGIN
         AND c.id_canton = NVL(pIdCanton, c.id_canton)
         AND pr.id_province = NVL(pIdProvince, pr.id_province)
         AND s.id_status = NVL(pIdStatus, s.id_status)
-        AND pt.id_pet_type = NVL(pIdPetType, pt.id_pet_type)
+        AND p.id_pet_type = NVL(pIdPetType, p.id_pet_type)
         AND r.id_race = NVL(pIdRace, r.id_race)
+        AND p.id_rescuer = NVL(pIdRescuer, p.id_rescuer)
         AND c.id_color = NVL(pIdColor, c.id_color);
 END getFilters;
