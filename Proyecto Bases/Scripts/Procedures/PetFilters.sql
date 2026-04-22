9b785dbfc2b1760e8f9d0b269482918fc96ff19c
Allan <allajox22@gmail.com>
2026-04-22 12:43:09 -0600

    PetFilters corrected
    Corrected issue of pet_x_district, changed the join of race and pet_type, changed many wrong names, and now the result is ordered by the creation date

--- Proyecto Bases/Scripts/Procedures/PetFilters.sql
+++ Proyecto Bases/Scripts/Procedures/PetFilters.sql
@@ -13,8 +13,11 @@ BEGIN
         INNER JOIN identification_chip ic
         ON p.id_pet = ic.id_pet
         
+        INNER JOIN pet_x_district pxd
+        ON p.id_pet = pxd.id_pet
+        
         INNER JOIN district d
-        ON p.id_pet = d.id_pet
+        ON pxd.id_pet = d.id_district
         
         INNER JOIN canton c
         ON d.id_canton = c.id_canton
@@ -29,13 +32,13 @@ BEGIN
         ON p.id_pet_type = pt.id_pet_type
         
         INNER JOIN race r
-        ON pt.id_race = r.id_race
+        ON pt.id_pet_type = r.id_pet_type
         
-        INNER JOIN color_x_pet cxp
-        ON p.id_pet = cxp.id_pet
+        INNER JOIN pet_x_color pxc
+        ON p.id_pet = pxc.id_pet
         
         INNER JOIN color c
-        ON cxp.id_color = c.id_color
+        ON pxc.id_color = c.id_color
         
         WHERE ic.id_chip = NVL(pIdChip, ic.id_chip)
         AND d.id_district = NVL(pIdDistrict, d.id_district)
@@ -45,5 +48,6 @@ BEGIN
         AND p.id_pet_type = NVL(pIdPetType, p.id_pet_type)
         AND r.id_race = NVL(pIdRace, r.id_race)
         AND p.id_rescuer = NVL(pIdRescuer, p.id_rescuer)
-        AND c.id_color = NVL(pIdColor, c.id_color);
+        AND c.id_color = NVL(pIdColor, c.id_color)
+        ORDER BY createdAt;
 END getFilters;
