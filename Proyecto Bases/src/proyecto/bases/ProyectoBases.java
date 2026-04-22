f4010719cbc234968cc8e767ac55521473196d35
Allan <allajox22@gmail.com>
2026-04-21 20:08:44 -0600

    Main

--- Proyecto Bases/src/proyecto/bases/ProyectoBases.java
+++ Proyecto Bases/src/proyecto/bases/ProyectoBases.java
@@ -15,7 +15,33 @@ public class ProyectoBases {
     public static void main(String[] args) throws SQLException{
         
         DBConnection.insertUser(1, "allan@gmail.com", "1234", "Allan", "5/4/2026", "Allan", "5/4/2026");
+        //DBConnection.insertUser(1, "allan@gmail.com", "1234", "Allan", "5/4/2026", "Allan", "5/4/2026");
+        /*
+        ImageIcon image = new ImageIcon("user.png");
         
+        JLabel label = new JLabel();
+        label.setText("ORAORAORAORAORAORAORA");
+        label.setIcon(image);
+        label.setHorizontalTextPosition(JLabel.CENTER); // set text relative to imageIcon
+        label.setVerticalTextPosition(JLabel.BOTTOM);
+        label.setForeground(Color.green); // text color
+        label.setFont(new Font("Times New Roman", Font.PLAIN, 20)); // text font
+        label.setVerticalAlignment(JLabel.CENTER);
+        label.setHorizontalAlignment(JLabel.CENTER);
+        label.setBounds(0, 0, 475, 475);
+        */
+
+        
+        
+        /*
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.white);
+        panel.setBounds(frame.getWidth()/6, 0, 500, 720);
+        panel.setLayout(null);
+        
+        panel.add(label);
+        frame.add(panel);
+        */
     }
     
 }
