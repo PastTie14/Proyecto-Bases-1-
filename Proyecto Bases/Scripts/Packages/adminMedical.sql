beb4c5c0769280aa86cf95d6cceb84c665abfd8f
Allan <allajox22@gmail.com>
2026-04-29 13:08:08 -0600

    Made update and get procedures from 2 packages

--- Proyecto Bases/Scripts/Packages/adminMedical.sql
+++ Proyecto Bases/Scripts/Packages/adminMedical.sql
@@ -1,5 +1,6 @@
 CREATE OR REPLACE PACKAGE adminMedical IS
 
+-- INSERT
 PROCEDURE insertTreatment(pIdTreatment IN NUMBER, pName IN VARCHAR2, pDose IN VARCHAR2);
 
 PROCEDURE insertDisease(pIdDisease IN NUMBER, pName IN VARCHAR2);
@@ -11,4 +12,22 @@ PROCEDURE insertDiseaseXMedicSheet(pIdTreatment IN NUMBER, pIdDisease IN NUMBER)
 
 PROCEDURE insertTreatmentXDisease(pIdTreatment IN NUMBER, pIdDisease IN NUMBER);
 
+-- UPDATE
+PROCEDURE updateTreatment(pIdTreatment IN NUMBER, pName IN VARCHAR2, pDose IN VARCHAR2);
+
+PROCEDURE updateDisease(pIdDisease IN NUMBER, pName IN VARCHAR2);
+
+PROCEDURE updateMedicSheet(pIdMedicSheet IN NUMBER, pAbandonmentDescription IN VARCHAR2,
+                                pIdVeterinarian IN NUMBER, pIdPetExtraInfo IN NUMBER);
+
+--PROCEDURE updateDiseaseXMedicSheet(pIdTreatment IN NUMBER, pIdDisease IN NUMBER);
+
+--PROCEDURE updateTreatmentXDisease(pIdTreatment IN NUMBER, pIdDisease IN NUMBER);
+
+-- GET
+FUNCTION getTreatment RETURN SYS_REFCURSOR;
+FUNCTION getDisease RETURN SYS_REFCURSOR;
+FUNCTION getMedicSheet RETURN SYS_REFCURSOR;
+FUNCTION getDiseaseXMedicSheet RETURN SYS_REFCURSOR;
+FUNCTION getTreatmentXDisease RETURN SYS_REFCURSOR;
 END adminMedical;
--- Proyecto Bases/Scripts/Packages/adminMedicalBody.sql
+++ Proyecto Bases/Scripts/Packages/adminMedicalBody.sql
@@ -50,4 +50,108 @@ BEGIN
     COMMIT;
 END insertTreatmentXDisease;
 
+
+-- ======================================== UPDATE ========================================
+PROCEDURE updateTreatment(pIdTreatment IN NUMBER, pName IN VARCHAR2, pDose IN VARCHAR2)
+IS
+BEGIN
+    UPDATE treatment
+    SET "name" = pName,
+        dose = pDose
+    
+    WHERE id_treatment = pIdTreatment;
+    COMMIT;
+END;
+
+PROCEDURE updateDisease(pIdDisease IN NUMBER, pName IN VARCHAR2)
+IS
+BEGIN
+    UPDATE disease
+    SET "name" = pName
+    
+    WHERE id_disease = pIdDisease;
+    COMMIT;
+END;
+
+PROCEDURE updateMedicSheet(pIdMedicSheet IN NUMBER, pAbandonmentDescription IN VARCHAR2,
+                                pIdVeterinarian IN NUMBER, pIdPetExtraInfo IN NUMBER)
+IS
+BEGIN
+    UPDATE medic_sheet
+    SET abandomnment_description = pAbandonmentDescription
+    
+    WHERE id_medic_sheet = pIdMedicSheet
+    AND id_veterinarian = pIdVeterinarian
+    AND id_pet_extra_info = pIdPetExtraInfo;
+    COMMIT;
+END;
+
+/*
+PROCEDURE updateDiseaseXMedicSheet(pIdTreatment IN NUMBER, pIdDisease IN NUMBER)
+IS
+BEGIN
+    UPDATE disease_x_medic_sheet
+    SET 
+    
+    WHERE ;
+    COMMIT;
+END;
+
+PROCEDURE updateTreatmentXDisease(pIdTreatment IN NUMBER, pIdDisease IN NUMBER)
+IS
+BEGIN
+    UPDATE treatment_x_disease
+    SET 
+    
+    WHERE ;
+    COMMIT;
+END;
+*/
+
+-- ======================================== GET ========================================
+FUNCTION getTreatment RETURN SYS_REFCURSOR
+IS
+    v_cursor SYS_REFCURSOR;
+    BEGIN
+        OPEN v_cursor FOR
+            SELECT * FROM treatment;
+        RETURN v_cursor;
+END;
+
+FUNCTION getDisease RETURN SYS_REFCURSOR
+IS
+    v_cursor SYS_REFCURSOR;
+    BEGIN
+        OPEN v_cursor FOR
+            SELECT * FROM disease;
+        RETURN v_cursor;
+END;
+
+FUNCTION getMedicSheet RETURN SYS_REFCURSOR
+IS
+    v_cursor SYS_REFCURSOR;
+    BEGIN
+        OPEN v_cursor FOR
+            SELECT * FROM medic_sheet;
+        RETURN v_cursor;
+END;
+
+FUNCTION getDiseaseXMedicSheet RETURN SYS_REFCURSOR
+IS
+    v_cursor SYS_REFCURSOR;
+    BEGIN
+        OPEN v_cursor FOR
+            SELECT * FROM disease_x_medic_sheet;
+        RETURN v_cursor;
+END;
+
+FUNCTION getTreatmentXDisease RETURN SYS_REFCURSOR
+IS
+    v_cursor SYS_REFCURSOR;
+    BEGIN
+        OPEN v_cursor FOR
+            SELECT * FROM treatment_x_disease;
+        RETURN v_cursor;
+END;
+
 END adminMedical;
