ac64f117a6ada87f255455d91f86b18d2d095744
Ian López Z <104876938+PastTie14@users.noreply.github.com>
2026-04-29 11:41:48 -0600

    modificado CatalogsPackage.sql

--- Proyecto Bases/Scripts/Packages/CatalogsPackage.sql
+++ Proyecto Bases/Scripts/Packages/CatalogsPackage.sql
@@ -1,13 +1,34 @@
-CREATE OR REPLACE PACKAGE adminCatalogs IS
+create or replace PACKAGE adminCatalogs IS
+    -- INSERT
+    PROCEDURE insertCurrency(p_id_currency IN NUMBER, p_name VARCHAR2, p_acronym VARCHAR2);
+    PROCEDURE insertProvince(p_id_province IN NUMBER, p_name VARCHAR2);
+    PROCEDURE insertCanton(p_id_canton IN NUMBER, p_name VARCHAR2, p_id_province IN NUMBER);
+    PROCEDURE insertDistrict(p_id_district IN NUMBER, p_name VARCHAR2, p_id_canton IN NUMBER);
+    PROCEDURE insertPetType(p_id_pet_type IN NUMBER, p_name VARCHAR2);
+    PROCEDURE insertRace(p_id_race IN NUMBER, p_name VARCHAR2, p_id_pet_type IN NUMBER);
+    PROCEDURE insertStatus(p_id_status IN NUMBER, p_status_type VARCHAR2);
+    PROCEDURE insertColor(p_id_color IN NUMBER, p_name VARCHAR2);
+    PROCEDURE insertValueType(p_id_value_type IN NUMBER, p_type VARCHAR2);
 
-PROCEDURE insertCurrency(id_currency IN NUMBER, "name" VARCHAR2,acronym VARCHAR2);
-PROCEDURE insertProvince(id_province IN NUMBER, "name" VARCHAR2);
-PROCEDURE insertCanton(id_canton IN NUMBER, "name" VARCHAR2,id_province IN NUMBER);
-PROCEDURE insertDistrict(id_district IN NUMBER, "name" VARCHAR2,id_canton IN NUMBER);
-PROCEDURE insertPetType(id_pet_type IN NUMBER, "name" VARCHAR2);
-PROCEDURE insertRace(id_race IN NUMBER, "name" VARCHAR2,id_pet_type IN NUMBER);
-PROCEDURE insertStatus(id_status IN NUMBER, status_type VARCHAR2);
-PROCEDURE insertColor(id_color IN NUMBER, "name" VARCHAR2);
-PROCEDURE insertValueType(id_value_type IN NUMBER, "type" VARCHAR2);
+    -- UPDATE
+    PROCEDURE updateCurrency(p_id_currency IN NUMBER, p_name VARCHAR2, p_acronym VARCHAR2);
+    PROCEDURE updateProvince(p_id_province IN NUMBER, p_name VARCHAR2);
+    PROCEDURE updateCanton(p_id_canton IN NUMBER, p_name VARCHAR2, p_id_province IN NUMBER);
+    PROCEDURE updateDistrict(p_id_district IN NUMBER, p_name VARCHAR2, p_id_canton IN NUMBER);
+    PROCEDURE updatePetType(p_id_pet_type IN NUMBER, p_name VARCHAR2);
+    PROCEDURE updateRace(p_id_race IN NUMBER, p_name VARCHAR2, p_id_pet_type IN NUMBER);
+    PROCEDURE updateStatus(p_id_status IN NUMBER, p_status_type VARCHAR2);
+    PROCEDURE updateColor(p_id_color IN NUMBER, p_name VARCHAR2);
+    PROCEDURE updateValueType(p_id_value_type IN NUMBER, p_type VARCHAR2);
 
+    -- GET
+    FUNCTION getCurrency RETURN SYS_REFCURSOR;
+    FUNCTION getProvince RETURN SYS_REFCURSOR;
+    FUNCTION getCanton RETURN SYS_REFCURSOR;
+    FUNCTION getDistrict RETURN SYS_REFCURSOR;
+    FUNCTION getPetType RETURN SYS_REFCURSOR;
+    FUNCTION getRace RETURN SYS_REFCURSOR;
+    FUNCTION getStatus RETURN SYS_REFCURSOR;
+    FUNCTION getColor RETURN SYS_REFCURSOR;
+    FUNCTION getValueType RETURN SYS_REFCURSOR;
 END adminCatalogs;
--- Proyecto Bases/Scripts/Packages/CatalogsPackageBody.sql
+++ Proyecto Bases/Scripts/Packages/CatalogsPackageBody.sql
@@ -1,75 +1,215 @@
-CREATE OR REPLACE PACKAGE BODY adminCatalogs AS
+create or replace PACKAGE BODY adminCatalogs AS
 
-PROCEDURE insertCurrency(id_currency IN NUMBER, "name" VARCHAR2,acronym VARCHAR2)
-IS 
-BEGIN
-    INSERT INTO currency
-    VALUES(id_currency,"name",acronym);
-    COMMIT;
-END;
+    -- ==================== INSERT ====================
 
-PROCEDURE insertProvince(id_province IN NUMBER, "name" VARCHAR2)
-IS 
-BEGIN
-    INSERT INTO province
-    VALUES(id_province,"name");
-    COMMIT;
-END;
+    PROCEDURE insertCurrency(p_id_currency IN NUMBER, p_name VARCHAR2, p_acronym VARCHAR2) IS
+    BEGIN
+        INSERT INTO currency VALUES(p_id_currency, p_name, p_acronym);
+        COMMIT;
+    END;
 
-PROCEDURE insertCanton(id_canton IN NUMBER, "name" VARCHAR2,id_province IN NUMBER)
-IS 
-BEGIN
-    INSERT INTO canton
-    VALUES(id_canton,"name",id_province);
-    COMMIT;
-END;
+    PROCEDURE insertProvince(p_id_province IN NUMBER, p_name VARCHAR2) IS
+    BEGIN
+        INSERT INTO province VALUES(p_id_province, p_name);
+        COMMIT;
+    END;
 
-PROCEDURE insertDistrict(id_district IN NUMBER, "name" VARCHAR2,id_canton IN NUMBER)
-IS 
-BEGIN
-    INSERT INTO district
-    VALUES(id_district,"name",id_canton);
-    COMMIT;
-END;
+    PROCEDURE insertCanton(p_id_canton IN NUMBER, p_name VARCHAR2, p_id_province IN NUMBER) IS
+    BEGIN
+        INSERT INTO canton VALUES(p_id_canton, p_name, p_id_province);
+        COMMIT;
+    END;
 
-PROCEDURE insertPetType(id_pet_type IN NUMBER, "name" VARCHAR2)
-IS 
-BEGIN
-    INSERT INTO pet_type
-    VALUES(id_pet_type,"name");
-    COMMIT;
-END;
+    PROCEDURE insertDistrict(p_id_district IN NUMBER, p_name VARCHAR2, p_id_canton IN NUMBER) IS
+    BEGIN
+        INSERT INTO district VALUES(p_id_district, p_name, p_id_canton);
+        COMMIT;
+    END;
 
-PROCEDURE insertRace(id_race IN NUMBER, "name" VARCHAR2,id_pet_type IN NUMBER)
-IS 
-BEGIN
-    INSERT INTO race
-    VALUES(id_race,"name",id_pet_type);
-    COMMIT;
-END;
+    PROCEDURE insertPetType(p_id_pet_type IN NUMBER, p_name VARCHAR2) IS
+    BEGIN
+        INSERT INTO pet_type VALUES(p_id_pet_type, p_name);
+        COMMIT;
+    END;
 
-PROCEDURE insertStatus(id_status IN NUMBER, status_type VARCHAR2)
-IS 
-BEGIN
-    INSERT INTO status
-    VALUES(id_status,status_type);
-    COMMIT;
-END;
+    PROCEDURE insertRace(p_id_race IN NUMBER, p_name VARCHAR2, p_id_pet_type IN NUMBER) IS
+    BEGIN
+        INSERT INTO race VALUES(p_id_race, p_name, p_id_pet_type);
+        COMMIT;
+    END;
 
-PROCEDURE insertColor(id_color IN NUMBER, "name" VARCHAR2)
-IS 
-BEGIN
-    INSERT INTO color
-    VALUES(id_color,"name");
-    COMMIT;
-END;
+    PROCEDURE insertStatus(p_id_status IN NUMBER, p_status_type VARCHAR2) IS
+    BEGIN
+        INSERT INTO status VALUES(p_id_status, p_status_type);
+        COMMIT;
+    END;
 
-PROCEDURE insertValueType(id_value_type IN NUMBER, "value" VARCHAR2)
-IS 
-BEGIN
-    INSERT INTO value_type
-    VALUES(id_value_type,"value");
-    COMMIT;
-END;
+    PROCEDURE insertColor(p_id_color IN NUMBER, p_name VARCHAR2) IS
+    BEGIN
+        INSERT INTO color VALUES(p_id_color, p_name);
+        COMMIT;
+    END;
+
+    PROCEDURE insertValueType(p_id_value_type IN NUMBER, p_type VARCHAR2) IS
+    BEGIN
+        INSERT INTO value_type VALUES(p_id_value_type, p_type);
+        COMMIT;
+    END;
+
+
+    -- ==================== UPDATE ====================
+
+    PROCEDURE updateCurrency(p_id_currency IN NUMBER, p_name VARCHAR2, p_acronym VARCHAR2) IS
+    BEGIN
+        UPDATE currency
+        SET    "name"    = p_name,
+               acronym = p_acronym
+        WHERE  id_currency = p_id_currency;
+        COMMIT;
+    END;
+
+    PROCEDURE updateProvince(p_id_province IN NUMBER, p_name VARCHAR2) IS
+    BEGIN
+        UPDATE province
+        SET    "name" = p_name
+        WHERE  id_province = p_id_province;
+        COMMIT;
+    END;
+
+    PROCEDURE updateCanton(p_id_canton IN NUMBER, p_name VARCHAR2, p_id_province IN NUMBER) IS
+    BEGIN
+        UPDATE canton
+        SET    "name"        = p_name,
+               id_province = p_id_province
+        WHERE  id_canton = p_id_canton;
+        COMMIT;
+    END;
+
+    PROCEDURE updateDistrict(p_id_district IN NUMBER, p_name VARCHAR2, p_id_canton IN NUMBER) IS
+    BEGIN
+        UPDATE district
+        SET    "name"      = p_name,
+               id_canton = p_id_canton
+        WHERE  id_district = p_id_district;
+        COMMIT;
+    END;
+
+    PROCEDURE updatePetType(p_id_pet_type IN NUMBER, p_name VARCHAR2) IS
+    BEGIN
+        UPDATE pet_type
+        SET    "name" = p_name
+        WHERE  id_pet_type = p_id_pet_type;
+        COMMIT;
+    END;
+
+    PROCEDURE updateRace(p_id_race IN NUMBER, p_name VARCHAR2, p_id_pet_type IN NUMBER) IS
+    BEGIN
+        UPDATE race
+        SET    "name"        = p_name,
+               id_pet_type = p_id_pet_type
+        WHERE  id_race = p_id_race;
+        COMMIT;
+    END;
+
+    PROCEDURE updateStatus(p_id_status IN NUMBER, p_status_type VARCHAR2) IS
+    BEGIN
+        UPDATE status
+        SET    status_type = p_status_type
+        WHERE  id_status = p_id_status;
+        COMMIT;
+    END;
+
+    PROCEDURE updateColor(p_id_color IN NUMBER, p_name VARCHAR2) IS
+    BEGIN
+        UPDATE color
+        SET    "name" = p_name
+        WHERE  id_color = p_id_color;
+        COMMIT;
+    END;
+
+    PROCEDURE updateValueType(p_id_value_type IN NUMBER, p_type VARCHAR2) IS
+    BEGIN
+        UPDATE value_type
+        SET    "type" = p_type
+        WHERE  id_value_type = p_id_value_type;
+        COMMIT;
+    END;
+
+
+    -- ==================== GET ====================
+
+    FUNCTION getCurrency RETURN SYS_REFCURSOR 
+    IS
+        v_cursor SYS_REFCURSOR;
+    BEGIN
+        OPEN v_cursor FOR
+            SELECT * FROM currency;
+        RETURN v_cursor;
+    END;
+    
+
+    FUNCTION getProvince RETURN SYS_REFCURSOR IS
+        v_cursor SYS_REFCURSOR;
+    BEGIN
+        OPEN v_cursor FOR
+            SELECT * FROM province;
+        RETURN v_cursor;
+    END;
+
+    FUNCTION getCanton RETURN SYS_REFCURSOR IS
+        v_cursor SYS_REFCURSOR;
+    BEGIN
+        OPEN v_cursor FOR
+            SELECT * FROM canton;
+        RETURN v_cursor;
+    END;
+
+    FUNCTION getDistrict RETURN SYS_REFCURSOR IS
+        v_cursor SYS_REFCURSOR;
+    BEGIN
+        OPEN v_cursor FOR
+            SELECT * FROM district;
+        RETURN v_cursor;
+    END;
+
+    FUNCTION getPetType RETURN SYS_REFCURSOR IS
+        v_cursor SYS_REFCURSOR;
+    BEGIN
+        OPEN v_cursor FOR
+            SELECT * FROM pet_type;
+        RETURN v_cursor;
+    END;
+
+    FUNCTION getRace RETURN SYS_REFCURSOR IS
+        v_cursor SYS_REFCURSOR;
+    BEGIN
+        OPEN v_cursor FOR
+            SELECT * FROM race;
+        RETURN v_cursor;
+    END;
+
+    FUNCTION getStatus RETURN SYS_REFCURSOR IS
+        v_cursor SYS_REFCURSOR;
+    BEGIN
+        OPEN v_cursor FOR
+            SELECT * FROM status;
+        RETURN v_cursor;
+    END;
+
+    FUNCTION getColor RETURN SYS_REFCURSOR IS
+        v_cursor SYS_REFCURSOR;
+    BEGIN
+        OPEN v_cursor FOR
+            SELECT * FROM color;
+        RETURN v_cursor;
+    END;
+
+    FUNCTION getValueType RETURN SYS_REFCURSOR IS
+        v_cursor SYS_REFCURSOR;
+    BEGIN
+        OPEN v_cursor FOR
+            SELECT * FROM value_type;
+        RETURN v_cursor;
+    END;
 
 END adminCatalogs;
