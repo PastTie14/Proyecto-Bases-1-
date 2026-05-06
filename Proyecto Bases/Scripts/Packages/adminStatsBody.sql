ab6525a8d26fe67fbd33e636590028fcd1237130
Allan <allajox22@gmail.com>
2026-05-05 18:31:55 -0600

    3 stat queries finished, added check in donation
    getPetsByTypeAndStatus may not be fully correct

--- Proyecto Bases/Scripts/Packages/adminStatsBody.sql
+++ Proyecto Bases/Scripts/Packages/adminStatsBody.sql
@@ -1,39 +1,58 @@
-CREATE OR REPLACE PACKAGE BODY adminStatsBody AS
+CREATE OR REPLACE PACKAGE BODY adminStats AS
     
-FUNCTION getPetsByTypeAndStatus(pIdType IN NUMBER, pIdStatus IN NUMBER) RETURN SYS_REFCURSOR IS
+FUNCTION getPetsByTypeAndStatus(pIdType IN NUMBER, pIdStatus IN NUMBER,
+                                    pStartDate IN DATE, pEndDate IN DATE) RETURN SYS_REFCURSOR IS
     v_cursor SYS_REFCURSOR;
     BEGIN
         OPEN v_cursor FOR
-            SELECT p.id_type, p.id_status FROM pet p
+            SELECT pt.id_pet_type, pt."name", s.id_status, s.status_type, 
+                    COUNT(p.id_pet) AS pet_count FROM pet_type pt
+                    
+            CROSS JOIN status s
             
-            WHERE p.id_type = NVL(pIdType, p.id_type)
-            AND p.id_status = NVL(pIdStatus, p.id_status);
-            -- ORDER BY;
+            LEFT JOIN pet p 
+            ON pt.id_pet_type = p.id_pet_type
+            AND s.id_status = p.id_status
+            AND p.createdAt BETWEEN NVL(pStartDate, TRUNC(SYSDATE, 'YYYY')) 
+                                     AND NVL(pEndDate, SYSDATE)
+            WHERE pt.id_pet_type = NVL(pIdType, pt.id_pet_type)
+            AND s.id_status = NVL(pIdStatus, s.id_status)
+            
+            GROUP BY pt.id_pet_type, pt."name", s.id_status, s.status_type
+            ORDER BY pt."name", s.status_type;
         RETURN v_cursor;
     END;
 
-FUNCTION getDonationsByAssociation(pIdDonation IN NUMBER, pIdAssociation IN NUMBER) RETURN SYS_REFCURSOR IS
+FUNCTION getDonationsByAssociation(pStartDate IN DATE, pEndDate IN DATE) RETURN SYS_REFCURSOR IS
     v_cursor SYS_REFCURSOR;    
     BEGIN
         OPEN v_cursor FOR
-            SELECT d.id_donation, d.id_association FROM donation d
+            SELECT a.id_user, a."name", COUNT (d.id_donation) AS donation_count FROM association a
             
-            WHERE d.id_donation = NVL(pIdDonation, d.id_donation)
-            AND d.id_association = NVL(pIdAssociation, d.id_association);
-            -- ORDER BY;
+            LEFT JOIN donation d -- LEFT to include associations without donations
+            ON a.id_user = d.id_association
+            -- WHERE isn't here to avoid invalidating the LEFT JOIN
+            AND d.created_at BETWEEN NVL(pStartDate, TRUNC(SYSDATE, 'YYYY')) -- default: start of this year
+                                                    AND NVL(pEndDate, SYSDATE) -- default: today
+            GROUP BY a.id_user, a."name"    
+            ORDER BY a."name";
         RETURN v_cursor;
     END;
     
-FUNCTION getDonationsByCribHouse(pIdDonation IN NUMBER, pIdCribHouse IN NUMBER) RETURN SYS_REFCURSOR IS
+FUNCTION getDonationsByCribHouse(pStartDate IN DATE, pEndDate IN DATE) RETURN SYS_REFCURSOR IS
     v_cursor SYS_REFCURSOR;    
     BEGIN
         OPEN v_cursor FOR
-            SELECT d.id_donation, d.id_crib_house FROM donation d
+            SELECT cb.id_user, cb."name", COUNT (d.id_donation) AS donation_count FROM crib_house cb
             
-            WHERE d.id_donation = NVL(pIdDonation, d.id_donation)
-            AND d.id_crib_house = NVL(pIdCribHouse, d.id_crib_house);
-            -- ORDER BY;
+            LEFT JOIN donation d -- LEFT to include crib houses without donations
+            ON cb.id_user = d.id_crib_house
+            -- WHERE isn't here to avoid invalidating the LEFT JOIN
+            AND d.created_at BETWEEN NVL(pStartDate, TRUNC(SYSDATE, 'YYYY')) -- default: start of this year
+                                                    AND NVL(pEndDate, SYSDATE) -- default: today
+            GROUP BY cb.id_user, cb."name"    
+            ORDER BY cb."name";
         RETURN v_cursor;
     END;
 
-END adminStatsBody;
+END adminStats;
