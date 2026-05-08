9c12941f6e52965c4dc7bb427ea47569735483a0
Allan <allajox22@gmail.com>
2026-05-08 17:50:31 -0600

    Catalogs comments

--- Proyecto Bases/Scripts/Tables/01_catalogs.sql
+++ Proyecto Bases/Scripts/Tables/01_catalogs.sql
@@ -1,9 +1,3 @@
--- ============================================================
--- FILE 01 - CATALOGS
--- Tables: currency, province, canton, district,
---         pet_type, race, status, color, value_type, size, size_x_crib_house
--- ============================================================
-
 -- ============================================
 -- CURRENCY
 -- ============================================
@@ -11,7 +5,11 @@ CREATE TABLE currency
 (
     id_currency NUMBER(4),
     "name"      VARCHAR2(20),
-    acronym     VARCHAR2(5)
+    acronym     VARCHAR2(5),
+    createdBy  VARCHAR2(20),
+    createdAt  DATE,
+    modifiedBy VARCHAR2(20),
+    modifiedAt DATE
 )
 TABLESPACE TS_DATA;
 
@@ -27,6 +25,30 @@ ALTER TABLE currency
 ALTER TABLE currency
     ADD CONSTRAINT pk_currency PRIMARY KEY (id_currency)
     USING INDEX TABLESPACE TS_INDEX;
+    
+COMMENT ON TABLE currency 
+IS 'Stores currency types for donations';
+
+COMMENT ON COLUMN currency.id_currency 
+IS 'Primary key, identifier for the currency';
+
+COMMENT ON COLUMN currency."name" 
+IS 'Full name of the currency';
+
+COMMENT ON COLUMN currency.acronym 
+IS 'Standard 3 letter acronym for the currency (USD, EUR, CRC)';
+
+COMMENT ON COLUMN currency.CreatedBy
+IS 'The user who created the table';
+
+COMMENT ON COLUMN currency.CreatedAt
+IS 'The date the table was created';
+
+COMMENT ON COLUMN currency.ModifiedBy
+IS 'The user who modified the table';
+
+COMMENT ON COLUMN currency.ModifiedAt
+IS 'The date the table was modified';
 
 -- ============================================
 -- PROVINCE
@@ -34,7 +56,11 @@ ALTER TABLE currency
 CREATE TABLE province
 (
     id_province NUMBER(4),
-    "name"      VARCHAR2(50)
+    "name"      VARCHAR2(50),
+    createdBy  VARCHAR2(20),
+    createdAt  DATE,
+    modifiedBy VARCHAR2(20),
+    modifiedAt DATE
 )
 TABLESPACE TS_DATA;
 
@@ -48,6 +74,27 @@ ALTER TABLE province
     ADD CONSTRAINT pk_province PRIMARY KEY (id_province)
     USING INDEX TABLESPACE TS_INDEX;
 
+COMMENT ON TABLE province 
+IS 'Stores the province';
+
+COMMENT ON COLUMN province.id_province 
+IS 'Primary key, identifier for the province';
+
+COMMENT ON COLUMN province."name" 
+IS 'Official name of the province';
+
+COMMENT ON COLUMN province.CreatedBy
+IS 'The user who created the table';
+
+COMMENT ON COLUMN province.CreatedAt
+IS 'The date the table was created';
+
+COMMENT ON COLUMN province.ModifiedBy
+IS 'The user who modified the table';
+
+COMMENT ON COLUMN province.ModifiedAt
+IS 'The date the table was modified';
+
 -- ============================================
 -- CANTON
 -- ============================================
@@ -55,7 +102,11 @@ CREATE TABLE canton
 (
     id_canton   NUMBER(4),
     "name"      VARCHAR2(50),
-    id_province NUMBER(4)
+    id_province NUMBER(4),
+    createdBy  VARCHAR2(20),
+    createdAt  DATE,
+    modifiedBy VARCHAR2(20),
+    modifiedAt DATE
 )
 TABLESPACE TS_DATA;
 
@@ -77,6 +128,30 @@ ALTER TABLE canton
     FOREIGN KEY (id_province) REFERENCES province (id_province)
     ON DELETE CASCADE;
 
+COMMENT ON TABLE canton 
+IS 'Stores the canton. Each province contains multiple cantons';
+
+COMMENT ON COLUMN canton.id_canton 
+IS 'Primary key, identifier for the canton';
+
+COMMENT ON COLUMN canton."name" 
+IS 'Official name of the canton';
+
+COMMENT ON COLUMN canton.id_province 
+IS 'Foreign key, references the province that contains this canton';
+
+COMMENT ON COLUMN canton.CreatedBy
+IS 'The user who created the table';
+
+COMMENT ON COLUMN canton.CreatedAt
+IS 'The date the table was created';
+
+COMMENT ON COLUMN canton.ModifiedBy
+IS 'The user who modified the table';
+
+COMMENT ON COLUMN canton.ModifiedAt
+IS 'The date the table was modified';
+
 -- ============================================
 -- DISTRICT
 -- ============================================
@@ -84,7 +159,11 @@ CREATE TABLE district
 (
     id_district NUMBER(4),
     "name"      VARCHAR2(50),
-    id_canton   NUMBER(4)
+    id_canton   NUMBER(4),
+    createdBy  VARCHAR2(20),
+    createdAt  DATE,
+    modifiedBy VARCHAR2(20),
+    modifiedAt DATE
 )
 TABLESPACE TS_DATA;
 
@@ -106,13 +185,41 @@ ALTER TABLE district
     FOREIGN KEY (id_canton) REFERENCES canton (id_canton)
     ON DELETE CASCADE;
 
+COMMENT ON TABLE district 
+IS 'Stores districts. Each canton has multiple districts';
+
+COMMENT ON COLUMN district.id_district 
+IS 'Primary key, identifier for the district';
+
+COMMENT ON COLUMN district."name" 
+IS 'Official name of the district';
+
+COMMENT ON COLUMN district.id_canton 
+IS 'Foreign key, references the canton that contains this district';
+
+COMMENT ON COLUMN district.CreatedBy
+IS 'The user who created the table';
+
+COMMENT ON COLUMN district.CreatedAt
+IS 'The date the table was created';
+
+COMMENT ON COLUMN district.ModifiedBy
+IS 'The user who modified the table';
+
+COMMENT ON COLUMN district.ModifiedAt
+IS 'The date the table was modified';
+
 -- ============================================
 -- PET TYPE
 -- ============================================
 CREATE TABLE pet_type
 (
     id_pet_type NUMBER(4),
-    "name"      VARCHAR2(50)
+    "name"      VARCHAR2(50),
+    createdBy  VARCHAR2(20),
+    createdAt  DATE,
+    modifiedBy VARCHAR2(20),
+    modifiedAt DATE
 )
 TABLESPACE TS_DATA;
 
@@ -126,6 +233,27 @@ ALTER TABLE pet_type
     ADD CONSTRAINT pk_pet_type PRIMARY KEY (id_pet_type)
     USING INDEX TABLESPACE TS_INDEX;
 
+COMMENT ON TABLE pet_type 
+IS 'Stores species of pets';
+
+COMMENT ON COLUMN pet_type.id_pet_type 
+IS 'Primary key, identifier for the pet type';
+
+COMMENT ON COLUMN pet_type."name" 
+IS 'Name of the pet category';
+
+COMMENT ON COLUMN pet_type.CreatedBy
+IS 'The user who created the table';
+
+COMMENT ON COLUMN pet_type.CreatedAt
+IS 'The date the table was created';
+
+COMMENT ON COLUMN pet_type.ModifiedBy
+IS 'The user who modified the table';
+
+COMMENT ON COLUMN pet_type.ModifiedAt
+IS 'The date the table was modified';
+
 -- ============================================
 -- RACE
 -- ============================================
@@ -133,7 +261,11 @@ CREATE TABLE race
 (
     id_race     NUMBER(4),
     "name"      VARCHAR2(50),
-    id_pet_type NUMBER(4)
+    id_pet_type NUMBER(4),
+    createdBy  VARCHAR2(20),
+    createdAt  DATE,
+    modifiedBy VARCHAR2(20),
+    modifiedAt DATE
 )
 TABLESPACE TS_DATA;
 
@@ -155,13 +287,41 @@ ALTER TABLE race
     FOREIGN KEY (id_pet_type) REFERENCES pet_type (id_pet_type)
     ON DELETE CASCADE;
 
+COMMENT ON TABLE race 
+IS 'Specific breed within a pet type';
+
+COMMENT ON COLUMN race.id_race 
+IS 'Primary key, identifier for the breed';
+
+COMMENT ON COLUMN race."name" 
+IS 'Name of the breed';
+
+COMMENT ON COLUMN race.id_pet_type 
+IS 'Foreign key, references the pet type this breed belongs to';
+
+COMMENT ON COLUMN race.CreatedBy
+IS 'The user who created the table';
+
+COMMENT ON COLUMN race.CreatedAt
+IS 'The date the table was created';
+
+COMMENT ON COLUMN race.ModifiedBy
+IS 'The user who modified the table';
+
+COMMENT ON COLUMN race.ModifiedAt
+IS 'The date the table was modified';
+
 -- ============================================
 -- STATUS
 -- ============================================
 CREATE TABLE status
 (
     id_status   NUMBER(4),
-    status_type VARCHAR2(30)
+    status_type VARCHAR2(30),
+    createdBy  VARCHAR2(20),
+    createdAt  DATE,
+    modifiedBy VARCHAR2(20),
+    modifiedAt DATE
 )
 TABLESPACE TS_DATA;
 
@@ -175,13 +335,38 @@ ALTER TABLE status
     ADD CONSTRAINT pk_status PRIMARY KEY (id_status)
     USING INDEX TABLESPACE TS_INDEX;
 
+COMMENT ON TABLE status 
+IS 'Stores statuses for pets';
+
+COMMENT ON COLUMN status.id_status 
+IS 'Primary key, identifier for the status';
+
+COMMENT ON COLUMN status.status_type 
+IS 'Name of the status (Adopted, Not adopted, Lost, Found)';
+
+COMMENT ON COLUMN status.CreatedBy
+IS 'The user who created the table';
+
+COMMENT ON COLUMN status.CreatedAt
+IS 'The date the table was created';
+
+COMMENT ON COLUMN status.ModifiedBy
+IS 'The user who modified the table';
+
+COMMENT ON COLUMN status.ModifiedAt
+IS 'The date the table was modified';
+
 -- ============================================
 -- COLOR
 -- ============================================
 CREATE TABLE color
 (
     id_color NUMBER(4),
-    "name"   VARCHAR2(30)
+    "name"   VARCHAR2(30),
+    createdBy  VARCHAR2(20),
+    createdAt  DATE,
+    modifiedBy VARCHAR2(20),
+    modifiedAt DATE
 )
 TABLESPACE TS_DATA;
 
@@ -195,13 +380,38 @@ ALTER TABLE color
     ADD CONSTRAINT pk_color PRIMARY KEY (id_color)
     USING INDEX TABLESPACE TS_INDEX;
 
+COMMENT ON TABLE color 
+IS 'Stores colors for pets';
+
+COMMENT ON COLUMN color.id_color 
+IS 'Primary key, identifier for the color';
+
+COMMENT ON COLUMN color."name" 
+IS 'Name of the color';
+
+COMMENT ON COLUMN color.CreatedBy
+IS 'The user who created the table';
+
+COMMENT ON COLUMN color.CreatedAt
+IS 'The date the table was created';
+
+COMMENT ON COLUMN color.ModifiedBy
+IS 'The user who modified the table';
+
+COMMENT ON COLUMN color.ModifiedAt
+IS 'The date the table was modified';
+
 -- ============================================
 -- VALUE TYPE
 -- ============================================
 CREATE TABLE value_type
 (
     id_value_type NUMBER(4),
-    "type"        VARCHAR2(30)
+    "type"        VARCHAR2(30),
+    createdBy  VARCHAR2(20),
+    createdAt  DATE,
+    modifiedBy VARCHAR2(20),
+    modifiedAt DATE
 )
 TABLESPACE TS_DATA;
 
@@ -215,13 +425,38 @@ ALTER TABLE value_type
     ADD CONSTRAINT pk_value_type PRIMARY KEY (id_value_type)
     USING INDEX TABLESPACE TS_INDEX;
     
+COMMENT ON TABLE value_type 
+IS 'Stores types of values for parameters';
+
+COMMENT ON COLUMN value_type.id_value_type 
+IS 'Primary key, identifier for the value type';
+
+COMMENT ON COLUMN value_type."type" 
+IS 'Name of the value type';
+
+COMMENT ON COLUMN value_type.CreatedBy
+IS 'The user who created the table';
+
+COMMENT ON COLUMN value_type.CreatedAt
+IS 'The date the table was created';
+
+COMMENT ON COLUMN value_type.ModifiedBy
+IS 'The user who modified the table';
+
+COMMENT ON COLUMN value_type.ModifiedAt
+IS 'The date the table was modified';
+
 -- ============================================
 -- SIZE
 -- ============================================
 CREATE TABLE "size"
 (
     id_size     NUMBER(1),
-    "name"      VARCHAR2(20)
+    "name"      VARCHAR2(20),
+    createdBy  VARCHAR2(20),
+    createdAt  DATE,
+    modifiedBy VARCHAR2(20),
+    modifiedAt DATE
 )
 TABLESPACE TS_DATA;
 
@@ -232,13 +467,38 @@ ALTER TABLE "size"
     ADD CONSTRAINT pk_size PRIMARY KEY (id_size)
     USING INDEX TABLESPACE TS_INDEX;
     
+COMMENT ON TABLE "size" 
+IS 'Stores pet sizes';
+
+COMMENT ON COLUMN "size".id_size 
+IS 'Primary key, identifier for pet size';
+
+COMMENT ON COLUMN "size"."name" 
+IS 'Name of the size';
+
+COMMENT ON COLUMN "size".CreatedBy
+IS 'The user who created the table';
+
+COMMENT ON COLUMN "size".CreatedAt
+IS 'The date the table was created';
+
+COMMENT ON COLUMN "size".ModifiedBy
+IS 'The user who modified the table';
+
+COMMENT ON COLUMN "size".ModifiedAt
+IS 'The date the table was modified';
+
 -- ============================================
 -- SIZE_X_CRIB_HOUSE
 -- ============================================
 CREATE TABLE size_x_crib_house
 (
     id_size         NUMBER(1),
-    id_crib_house   NUMBER(8)
+    id_crib_house   NUMBER(8),
+    createdBy       VARCHAR2(20),
+    createdAt       DATE,
+    modifiedBy      VARCHAR2(20),
+    modifiedAt      DATE
 )
 TABLESPACE TS_DATA;
 
@@ -255,3 +515,24 @@ ALTER TABLE size_x_crib_house
     ADD CONSTRAINT fk_sxch_crib_house
     FOREIGN KEY (id_crib_house) REFERENCES crib_house (id_user)
     ON DELETE CASCADE;
+
+COMMENT ON TABLE size_x_crib_house 
+IS 'Intermediate table, N to N relationship between size and crib_house';
+
+COMMENT ON COLUMN size_x_crib_house.id_size 
+IS 'Composite primary key. Foreign key references the size id';
+
+COMMENT ON COLUMN size_x_crib_house.id_crib_house 
+IS 'Composite primary key. Foreign key references the crib house id';
+
+COMMENT ON COLUMN size_x_crib_house.CreatedBy
+IS 'The user who created the table';
+
+COMMENT ON COLUMN size_x_crib_house.CreatedAt
+IS 'The date the table was created';
+
+COMMENT ON COLUMN size_x_crib_house.ModifiedBy
+IS 'The user who modified the table';
+
+COMMENT ON COLUMN size_x_crib_house.ModifiedAt
+IS 'The date the table was modified';
