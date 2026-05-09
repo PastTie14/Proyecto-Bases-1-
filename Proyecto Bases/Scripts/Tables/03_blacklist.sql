8cbeea34ea736c2aaf770165556387770d998661
Allan <allajox22@gmail.com>
2026-05-08 18:38:29 -0600

    black_list comments

--- Proyecto Bases/Scripts/Tables/03_blacklist.sql
+++ Proyecto Bases/Scripts/Tables/03_blacklist.sql
@@ -28,6 +28,27 @@ ALTER TABLE black_list
     ADD CONSTRAINT fk_black_list_user
     FOREIGN KEY (id_user) REFERENCES "user" (id_user);
 
+COMMENT ON TABLE black_list
+IS 'Stores the black list information';
+
+COMMENT ON COLUMN black_list.id_report
+IS 'Primary key, identifier for the black list id';
+
+COMMENT ON COLUMN black_list.id_user
+IS 'Foreign key, references the user who owns the black list';
+
+COMMENT ON COLUMN black_list.CreatedBy
+IS 'The user who created the table';
+
+COMMENT ON COLUMN black_list.CreatedAt
+IS 'The date the table was created';
+
+COMMENT ON COLUMN black_list.ModifiedBy
+IS 'The user who modified the table';
+
+COMMENT ON COLUMN black_list.ModifiedAt
+IS 'The date the table was modified';
+
 -- ============================================
 -- USER X BLACK LIST
 -- ============================================
@@ -61,3 +82,27 @@ ALTER TABLE user_x_black_list
     ADD CONSTRAINT fk_uxbl_report
     FOREIGN KEY (id_report) REFERENCES black_list (id_report)
     ON DELETE CASCADE;
+
+COMMENT ON TABLE user_x_black_list
+IS 'Intermediate table, stores the blocked users from a black list';
+
+COMMENT ON COLUMN user_x_black_list.reason
+IS 'Reason why the user was blocked';
+
+COMMENT ON COLUMN user_x_black_list.id_report
+IS 'Composite Primary key. Foreign key references the id of the black list';
+
+COMMENT ON COLUMN user_x_black_list.id_user
+IS 'Composite Foreign key. Foreign key references the id of the blocked user';
+
+COMMENT ON COLUMN user_x_black_list.CreatedBy
+IS 'The user who created the table';
+
+COMMENT ON COLUMN user_x_black_list.CreatedAt
+IS 'The date the table was created';
+
+COMMENT ON COLUMN user_x_black_list.ModifiedBy
+IS 'The user who modified the table';
+
+COMMENT ON COLUMN user_x_black_list.ModifiedAt
+IS 'The date the table was modified';
