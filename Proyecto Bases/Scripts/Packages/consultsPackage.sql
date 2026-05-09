9b0e141882c9463ee12cfc308a18f112b8d6e541
Allan <allajox22@gmail.com>
2026-05-09 00:25:42 -0600

    Made getDonations function

--- Proyecto Bases/Scripts/Packages/consultsPackage.sql
+++ Proyecto Bases/Scripts/Packages/consultsPackage.sql
@@ -2,16 +2,16 @@ CREATE OR REPLACE PACKAGE adminConsult AS
  
     FUNCTION getDonations(pStartDate IN DATE, pEndDate IN DATE, pIdDonor IN NUMBER, pIdAssociation IN NUMBER) RETURN SYS_REFCURSOR;
  
-    FUNCTION getBlackListReport RETURN SYS_REFCURSOR;
+    --FUNCTION getBlackListReport RETURN SYS_REFCURSOR;
  
-    FUNCTION getBlackListReportDetails(pIdUser IN NUMBER) RETURN SYS_REFCURSOR;
+    --FUNCTION getBlackListReportDetails(pIdUser IN NUMBER) RETURN SYS_REFCURSOR;
 
-    FUNCTION getMatches(pIdType IN NUMBER,pIdRace IN NUMBER) RETURN SYS_REFCURSOR;
+    --FUNCTION getMatches(pIdType IN NUMBER,pIdRace IN NUMBER) RETURN SYS_REFCURSOR;
 
-    FUNCTION getPetNecessaryTreatments(pMin IN NUMBER, pMax IN NUMBER) RETURN SYS_REFCURSOR;
+    --FUNCTION getPetNecessaryTreatments(pMin IN NUMBER, pMax IN NUMBER) RETURN SYS_REFCURSOR;
 
-    FUNCTION getCompatibleCribHouses(pIdPetType IN NUMBER) RETURN SYS_REFCURSOR;
+    --FUNCTION getCompatibleCribHouses(pIdPetType IN NUMBER) RETURN SYS_REFCURSOR;
 
-    FUNCTION getBestRescuersAndAdopters(pStartDate IN DATE,pEndDate   IN DATE) RETURN SYS_REFCURSOR;
+    --FUNCTION getBestRescuersAndAdopters(pStartDate IN DATE,pEndDate   IN DATE) RETURN SYS_REFCURSOR;
  
 END adminConsult;
--- Proyecto Bases/Scripts/Packages/consultsPackageBody.sql
+++ Proyecto Bases/Scripts/Packages/consultsPackageBody.sql
@@ -1,3 +1,25 @@
 CREATE OR REPLACE PACKAGE BODY adminConsult AS
 
+FUNCTION getDonations(pStartDate IN DATE, pEndDate IN DATE, pIdDonor IN NUMBER, 
+                        pIdAssociation IN NUMBER) RETURN SYS_REFCURSOR
+IS
+    v_cursor SYS_REFCURSOR;
+    BEGIN
+        OPEN v_cursor FOR
+        SELECT d.amount, d.id_donnor, d.createdAt, a."name" FROM donation d
+        
+        INNER JOIN association a
+        ON d.id_association = a.id_user
+        
+        WHERE d.createdAt BETWEEN NVL(pStartDate, TRUNC(SYSDATE, 'YYYY')) -- default: start of this year
+                                     AND NVL(pEndDate, SYSDATE) -- default: today
+        AND d.id_donnor = NVL(pIdDonor, d.id_donnor)
+        AND d.id_association = NVL(pIdAssociation, d.id_association)
+        
+        GROUP BY d.amount, d.id_donnor, d.createdAt, a."name"
+        ORDER BY d.amount;
+
+        RETURN v_cursor;
+    END;
+
 END adminConsult;
