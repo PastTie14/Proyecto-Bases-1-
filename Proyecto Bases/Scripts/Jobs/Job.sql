9c3b4b1971b84acd1f21d7e83eef949839ba3e5e
adrrod142857 <adrrod4907@gmail.com>
2026-05-09 19:32:25 -0600

    Job finalizado
    Yay

--- Proyecto Bases/Scripts/Jobs/Job.sql
+++ Proyecto Bases/Scripts/Jobs/Job.sql
@@ -9,7 +9,35 @@ DBMS_SCHEDULER.CREATE_JOB
 (
     job_name => 'MATCH_FOUND_LOST_JOB',
     job_type => 'PLSQL_BLOCK',
-    job_action => '',
+    job_action => 'BEGIN
+    INSERT INTO "MATCH"
+    (
+        id_match,
+        match_date,
+        id_pet_lost,
+        id_pet_found
+    )
+    SELECT
+        s_match.NEXTVAL,
+        SYSDATE,
+        p.id_pet,
+        q.id_pet
+    FROM pet p
+    JOIN pet q
+        ON p.id_race = q.id_race
+    LEFT JOIN "MATCH" m1
+        ON m1.id_pet_lost = p.id_pet
+       AND m1.id_pet_found = q.id_pet
+    LEFT JOIN "MATCH" m2
+        ON m2.id_pet_lost = q.id_pet
+       AND m2.id_pet_found = p.id_pet
+    WHERE p.id_pet < q.id_pet
+      AND p.date_lost IS NOT NULL
+      AND q.date_found IS NOT NULL
+      AND q.date_found > p.date_lost
+      AND m1.id_match IS NULL
+      AND m2.id_match IS NULL;
+END;',
     schedule_name => 'schedule_jobs',
     enabled = false
 );
